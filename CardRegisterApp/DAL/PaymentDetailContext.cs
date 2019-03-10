a0dc89ae72196235fbe656fc4f95f28c1d38a927
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-03-09 19:53:32 -0600

    Add paymentDetail model & Re-locate the context class

---
+++ CardRegisterApp/DAL/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardRegisterApp.API.DAL
+{
+    public class PaymentDetailContext : DbContext
+    {
+        private readonly DbContextOptions options;
+
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+            this.options = options;
+        }
+    }
+}
---
+++ CardRegisterApp/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardRegisterApp.API.Models
+{
+    public class PaymentDetail
+    {
+        //Payment Id
+        [Key]
+        public int PMId { get; set; }
+        [Required]
+        public string CardOwnerName { get; set; }
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public string ExpirationDate { get; set; }
+        [Required]
+        public string CVV { get; set; }
+    }
+}
